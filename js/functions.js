20627f16410ae5f95e4d94ca2d91d4307022b049
alex2005git <c.alex2005.mail@gmail.com>
2013-11-06 23:28:25 +0200

    functionality

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ function gotopage(where)
     var path = window.location.pathname;
     var page = path.substr( path.lastIndexOf("/") + 1 );
     
-    window.location = page + "?" + where;
+    window.location = page + (where != undefined ? "?" + where : '');
 }
 
 // returns number 1-52
@@ -13,7 +13,7 @@ function random_card()
     return Math.floor((Math.random()*card_count)+1);
 }
 
-// store/get local values
+// store/get/remove local values
 function storeValue(key, value) {
     if (localStorage) {
         localStorage.setItem(key, value);
@@ -21,7 +21,6 @@ function storeValue(key, value) {
         $.cookies.set(key, value);
     }
 }
-
 function getStoredValue(key) {
     if (localStorage) {
         return localStorage.getItem(key);
@@ -29,11 +28,33 @@ function getStoredValue(key) {
         return $.cookies.get(key);
     }
 }
+function removeValue(key) {
+    if (localStorage) {
+        localStorage.removeItem(key);
+    }
+}
+function clearValues() {
+    localStorage.clear();
+}
 
 
 // debug function, output to console
 function log(msg) {
     setTimeout(function() {
-        throw new Error(msg);
+        throw new Error("#" + msg +"#");
     }, 0);
 }
+function showStorage() {
+    for (var i = 0; i < localStorage.length; i++){
+        log("storage["+localStorage.key(i)+"]: " + localStorage.getItem(localStorage.key(i)));
+    }
+}
+
+// get associative array size(object)
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-var card_ids = new Array();
+var card_ids = {}; //{}
 var points = aces = 0;
 var showcards = '';
 
-// draw 2 random cards
-for (i = 0; i <= 1; i++)
-    card_ids[i] = random_card(); 
+if (getStoredValue('hitme'))
+{
+    card_ids = JSON.parse(getStoredValue('card_ids'));
+    card_ids[Object.size(card_ids)] = random_card();
+}
+else
+{
+    // draw 2 random cards
+    for (i = 0; i <= 1; i++)
+        card_ids[i] = random_card();
+}
 
 // for each card calculate the points
-for (i = 0; i < card_ids.length; i++)
+for (i = 0; i < Object.size(card_ids); i++)
 {
     // current card id
     var card_id = card_ids[i];
@@ -24,11 +32,83 @@ for (i = 0; i < card_ids.length; i++)
     
     showcards += "<img src='img/" + card_info[card_id]['card'] + ".bmp' border=0 /> ";
 }
-
 /* count aces
  ** points >= 11 then add 1, else 11
  */
 for (i = 0; i < aces; i++)
     points += (points < 11 && aces - i == 1 ? 11 : 1);
 
+function show_default()
+{
+    html_ouput =
+    '<table cellspacing=0 cellpadding=3 width=400>' +
+    '<tr><td colspan=2 cellspacing=0 cellpadding=5 align=center>' +
+    '<table width="100%" cellspacing=0 cellpadding=10 bgcolor=white>' +
+    '<tr><td align=center><img src="img/as.bmp" border=0>&nbsp;<img src="img/js.bmp"border=0></td></tr>' +
+    '<tr><td align=left>You must collect 21 points without going over.</td></tr>' +
+    '<tr><td align=center>' +
+    '<input type="button" value="Start!" onclick="storeValue(\'start\', 1); gotopage()">' +
+    '</td></tr></table>' +
+    '</td></tr></table>';
+    
+    return html_ouput;
+}
+
+function show_game()
+{
+    html_ouput =
+    '<table cellspacing=0 cellpadding=3 width=400>' +
+    '<tr><td colspan=2 cellspacing=0 cellpadding=5 align=center>' +
+    '<table width="100%" cellspacing=0 cellpadding=10 bgcolor=white>' +
+    '<tr><td align=center>' + showcards + '</td></tr>' +
+    '<tr><td align=center>Points = ' + points + '</td></tr>' +
+    '<tr><td align=center>' +
+    '<input type="button" value="Hit me!" onclick="storeValue(\'card_ids\', JSON.stringify(card_ids)); storeValue(\'hitme\', 1); gotopage()">';
+
+    if (points > 10)
+    {
+        html_ouput +=
+        '<input type="button" value="Stay" onclick="storeValue(\'stay\', 1); gotopage()">';
+    }
+
+    html_ouput +=
+    '</td></tr></table>' +
+    '</td></tr></table>';
+    
+    return html_ouput;
+}
+
+function show_gameover()
+{
+    html_ouput =
+    '<table cellspacing=0 cellpadding=3 width=400>' +
+    '<tr><td colspan=2 cellspacing=0 cellpadding=5 align=center>' +
+    '<table width="100%" cellspacing=0 cellpadding=10 bgcolor=white>' +
+    '<tr><td align=center>' + showcards + '</td></tr>' +
+    '<tr><td align=left>Points = ' + points + '</td></tr>' +
+    '<tr><td align=center>' +
+    '<input type="button" value="New Game!" onclick="clearValues(); gotopage()">' +
+    '</td></tr></table>' +
+    '</td></tr></table>';
+    
+    return html_ouput;
+}
+
+function show_stats()
+{
+    html_output =
+    '<br /><br /><br />' +
+
+    '<table cellspacing=0 cellpadding=3 width=400>' +
+    '<tr><td colspan=2 cellspacing=0 cellpadding=5 align=center>' +
+    '<div align="center"><h1>Personal Statistics</h1></div>' +
+    '<tr><td align=left><b>Wins</b></td><td align=center><b></b></td></tr>' +
+    '<tr><td align=left><b>Losses</b></td><td align=center><b></b></td></tr>' +
+    '<tr><td align=left><b>Games Played</b></td><td align=center><b></b></td></tr>' +
+    '<tr><td align=left><b>Win Percentage</b></td><td align=center><b></b></td></tr>' +
+    '<tr><td align=left><b>+/-</b></td><td align=center><b></b></td></tr>' +
+    '</td></tr></table>';
+
+    return html_output;
+}
  
